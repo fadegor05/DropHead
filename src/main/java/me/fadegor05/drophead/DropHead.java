f51ca25fe2347130554897c4c0b28f1031aef648
fadegor05 <fad.egor05@gmail.com>
2023-12-06 15:40:27 +0400

    #1 commit
    + Added onDeath event handler
    + Added getHead function
    
    * Bit refactored

--- src/main/java/me/fadegor05/drophead/DropHead.java
+++ src/main/java/me/fadegor05/drophead/DropHead.java
@@ -1,19 +1,38 @@
 package me.fadegor05.drophead;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
 
 import java.util.logging.Logger;
 
-public final class DropHead extends JavaPlugin {
+public final class DropHead extends JavaPlugin implements Listener {
 
     private static final Logger logger = Logger.getLogger("Minecraft");
-
-    private static DropHead plugin;
-
-    private static DropHead getPlugin() { return plugin; }
 
     @Override
     public void onEnable() {
         logger.info("DropHead 1.0 has been enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        player.getWorld().dropItem(player.getLocation(), getHead(player));
+    }
+
+    public static ItemStack getHead(Player player){
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta headMeta = (SkullMeta) head.getItemMeta();
+        headMeta.setPlayerProfile(player.getPlayerProfile());
+        head.setItemMeta(headMeta);
+        return head;
     }
 }
